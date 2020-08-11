9c50dbd0c7bc4c9890c37baaf73168aff37089cb
benjamin <benjamin.roullet@gmail.com>
2020-08-11 15:14:48 +0200

    :art: User env var for database url

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -4,6 +4,6 @@ export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect('mongodb://localhost/mangareader'),
+      mongoose.connect(process.env.DATABASE_URL || 'mongodb://localhost/mangareader'),
   }
 ];
